7ce7e37b857837e96516d2b0b8f86bc684e018b8
Gabbriel <74091198+Gabriel-Breshears@users.noreply.github.com>
2022-08-25 09:01:03 -0500

    create practice problems for python

---
+++ main.py
@@ -0,0 +1,111 @@
+# Hello Fellow Pythonista! Here are some practice problems for today
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+
+# 1. What will print to the console? another way to think about it, What will show up on the screen when you hit run?
+
+favorite_artist = 'Taylor Swift'
+
+print(favorite_artist)    #the print() function is what displays content on the screen.
+
+# 2. What will print to the console now?
+
+favorite_artist = 'Taylor Swift'
+favorite_artist = 'Oliver Tree'
+
+print(favorite_artist)
+
+
+# 3. You can assign variables the value of other variables
+# What will print to the console?
+
+edward_favorite_color = 'red'
+al_favorite_color = 'blue'
+
+edward_favorite_color = al_favorite_color
+
+print(edward_favorite_color)
+
+
+
+# 4 .  Look at the variables defined below:
+
+a = 50
+b = 5
+c = 10
+
+
+# Put the lines in the correct order to reach the following results:
+# Another way to phrase the question, assign the variables the values of the other variables to reach the result below
+
+a = 5
+b = 50
+c = 5
+
+
+# 5. What variable names are acceptable?
+#
+# Print one or more options from the list that have acceptable variable names.
+
+ my_name = 'Bob'
+
+ _joke = 'knock, knock...'
+
+ gr8 = 'great'
+
+ 50cent = 'the rapper or an amount of money'
+
+# 6.Choose the lines that do NOT contain errors.
+
+a. PRINT("Hello, World!")
+
+b. print("Hello, World!"
+
+c. print(Hello, World!)
+
+d. print("Hello, World!")
+
+e. print('Hello, World!')
+
+
+# 7. Write a program that prints the string "My name is " followed by your name like in the output example. All words in this string must be separated by a single space.
+
+# Output example
+#
+# My name is John
+
+# 8. The title of Alice's Adventures in Wonderland is often shortened to Alice in Wonderland.
+# Print the full title Alice's Adventures in Wonderland
+# Hint there are two ways to create strings in python
+
+
+
+# 9. Write a program that prints the following line.
+# 2 + 2 = 4
+
+
+
+#10 Try to solve this first before hitting run to check. Which print statements will crash your program / return an error.
+#
+ print("The ship's name is 'Brave'.")
+
+print('Cat's paws.')
+
+print('The word "Ciao" means "hello" in Italian.')
+
+print("And "Ciao" is Italian "Bye".")
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
